5219d15dce9f1d80a80b54cbb2b8aa7cb6e423d7
jiayanchang <jiayc@bbtree.com>
2017-04-17 22:54:17 +0800

    modify conf

--- test/initData.py
+++ test/initData.py
@@ -17,40 +17,45 @@ import time
 client = MongoClient(test_conf.timeline_mongo_host['host'])
 db = client.circle_info
 
-
-def insertUserToCircle(circleId, users):
-
-    record = db.circle_user_page.find({"circleId": ObjectId(circleId)}, {"circleId": 1}).limit(1)
-    if len(list(record)) > 0:
-        db.circle_user_page.update_one({"circleId": ObjectId(circleId), "pageIndex": 1}, {"$push": {"users": users}}, False)
-    else:
-        db.circle_user_page.insert_one({
-            "circleId": "58ede8ad0cf28b716ec4cb02",
-            "pageIndex": 1,
-            "pageCount": 10000,
-            "users": users
-        })
+def clearUserRelatives(users):
+    for userId in users:
+        db.user_circle_relationship.delete_many({"userId":userId})
 
 
-    record = db.user_circle_relationship.find({"userId": userId}).limit(1)
+def insertUserToCircle(circleId, users, index):
+    # record = db.circle_user_page.find({"circleId": ObjectId(circleId)}, {"circleId": 1}).limit(1)
+    # if len(list(record)) > 0:
+    # db.circle_user_page.delete_many({"circleId": ObjectId(circleId), "pageIndex": index})
 
-    if len(list(record)) > 0:
-        db.user_circle_relationship.update_one({"userId": userId}, {"$push": {"circles": {
-            "circleId": circleId,
-            "userRole": 0,
-            "joinTime": datetime.datetime.now()
-        }}}, False)
-    else:
-        db.user_circle_relationship.insert_one({
-            "userId": userId,
-            "circles": [
-                {
-                    "circleId": circleId,
-                    "userRole": 0,
-                    "joinTime": datetime.datetime.now()
-                }
-            ]
-        })
+    db.circle_user_page.insert_one({
+        "circleId": circleId,
+        "pageIndex": index,
+        "pageCount": 10000,
+        "users": users
+    })
+
+
+    for userId in users:
+        record = db.user_circle_relationship.find({"userId": userId}, {"userId": 1}).limit(1)
+        if len(list(record)) > 0:
+            db.user_circle_relationship.update_one({"userId": userId}, {"$push": {"circles": {
+                "circleId": circleId,
+                "userRole": 0,
+                "joinTime": datetime.datetime.now()
+            }}}, False)
+        else:
+            db.user_circle_relationship.insert_one({
+                "userId": userId,
+                "circles": [
+                    {
+                        "circleId": circleId,
+                        "userRole": 0,
+                        "joinTime": datetime.datetime.now()
+                    }
+                ]
+            })
+
+    print circleId, index
 
 
 circlefile = open("circleId.txt")
@@ -60,8 +65,8 @@ line = userfile.readline()
 circleId = circlefile.readline()
 
 while circleId:
-    print circleId
     users = []
+    index = 1
     while line:
         userId = line.split('\t')[0]
         users.append({"userId": userId, "userRole": 0, "mobile": str('18000000000'), "sex": -1, "joinTime": datetime.datetime.now()})
@@ -70,8 +75,8 @@ while circleId:
         if len(users) < 10000:
             continue
 
-        print len(users)
-        insertUserToCircle(circleId.replace('\n', ''), users)
+        insertUserToCircle(circleId.replace('\n', ''), users, index)
+        index += 1
         users = []
 
     circleId = circlefile.readline()
