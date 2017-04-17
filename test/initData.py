158f571180fe20eeb6ddbe734f363c4abf7eb39b
jiayanchang <jiayc@bbtree.com>
2017-04-17 23:28:30 +0800

    modify conf

--- test/initData.py
+++ test/initData.py
@@ -17,67 +17,65 @@ import time
 client = MongoClient(test_conf.timeline_mongo_host['host'])
 db = client.circle_info
 
-def clearUserRelatives(users):
-    for userId in users:
-        db.user_circle_relationship.delete_many({"userId":userId})
-
-
-def insertUserToCircle(circleId, users, index):
-    # record = db.circle_user_page.find({"circleId": ObjectId(circleId)}, {"circleId": 1}).limit(1)
-    # if len(list(record)) > 0:
-    # db.circle_user_page.delete_many({"circleId": ObjectId(circleId), "pageIndex": index})
-
-    db.circle_user_page.insert_one({
-        "circleId": circleId,
-        "pageIndex": index,
-        "pageCount": 10000,
-        "users": users
-    })
-
-
-    for userId in users:
-        record = db.user_circle_relationship.find({"userId": userId}, {"userId": 1}).limit(1)
-        if len(list(record)) > 0:
-            db.user_circle_relationship.update_one({"userId": userId}, {"$push": {"circles": {
-                "circleId": circleId,
-                "userRole": 0,
-                "joinTime": datetime.datetime.now()
-            }}}, False)
-        else:
-            db.user_circle_relationship.insert_one({
-                "userId": userId,
-                "circles": [
-                    {
-                        "circleId": circleId,
-                        "userRole": 0,
-                        "joinTime": datetime.datetime.now()
-                    }
-                ]
-            })
-
-    print circleId, index
-
-
 circlefile = open("circleId.txt")
 userfile = open("prod_10w.txt")
 
 line = userfile.readline()
-circleId = circlefile.readline()
+fcircleId = circlefile.readline()
 
-while circleId:
-    users = []
+circleIds = []
+while fcircleId:
+    circleIds.append(fcircleId.replace('\n', ''))
+    fcircleId = circlefile.readline()
+
+users = []
+while line:
+    userId = line.split('\t')[0]
+    users.append({"userId": userId, "userRole": 0, "mobile": str('18000000000'), "sex": -1, "joinTime": datetime.datetime.now()})
+    line = userfile.readline()
+
+userPages = []
+print 'circleIds', len(circleIds)
+print 'users', len(users)
+for circleId in circleIds:
     index = 1
-    while line:
-        userId = line.split('\t')[0]
-        users.append({"userId": userId, "userRole": 0, "mobile": str('18000000000'), "sex": -1, "joinTime": datetime.datetime.now()})
-        line = userfile.readline()
-
-        if len(users) < 10000:
+    temps = []
+    for user in users:
+        temps.append(user)
+        if len(temps) < 10000:
             continue
 
-        insertUserToCircle(circleId.replace('\n', ''), users, index)
+        userPages.append({
+            "circleId": circleId,
+            "pageIndex": index,
+            "pageCount": 10000,
+            "users": temps
+        })
         index += 1
-        users = []
+        temps = []
 
-    circleId = circlefile.readline()
+print 'userPages', len(userPages)
 
+relatives = []
+for user in users:
+    circles = []
+    for circleId in circleIds:
+        circles.append({
+            "circleId": circleId,
+            "userRole": 0,
+            "joinTime": datetime.datetime.now()
+        })
+
+    relatives.append({
+        "userId": userId,
+        "circles": circles
+    })
+
+print 'relatives', len(relatives)
+
+
+for userPage in userPages:
+    db.circle_user_page.insert_one(userPage)
+
+for relative in relatives:
+    db.user_circle_relationship.insert_one(relative)
